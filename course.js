62f2ae490fbad41ee73731e4af5ce8c7acb7f9f4
uknowlibrary <b4548114@gmail.com>
2025-12-04 14:52:10 +0530

    Update course.js

--- course.js
+++ course.js
@@ -20,13 +20,14 @@ document.getElementById("output").innerHTML="<p id='internaloutput'></p>";
 const cs = setTimeout(mycourse,1000);
 
 function mycourse(){
-document.getElementById("srch").innerHTML="<input type='text' id='mycourse' onkeyup='mycourseselect()' placeholder='Search for course..' title='Type in a name' style='max-width:200px;height:50px;border-radius:26px 40px;'>";
+document.getElementById("srch").innerHTML="<input type='text' id='myInput' onkeyup='mycourseselect()' placeholder='Search for course..' title='Type in a name' style='max-width:200px;height:50px;border-radius:26px 40px;'>";
 
 document.getElementById("coursehover").style.backgroundColor="gold";
 document.getElementById("coursehover").style.color="black";
 
 document.getElementById("internaloutput").innerHTML="<ul id='myULs'>                                                                                                                                  <lis><img src='/images/btech.png' alt='B.TECH' onclick='btech();' style='width:100px; height:150px;'><br><as href='#'>B.TECH</as></lis>                                                                                                                                                                                                                                                                                 <lis><img src='/images/diploma.png' alt='DIPLOMA' onclick='diploma();' style='width:100px; height:150px;'><br><as href='#'>DIPLOMA</as></lis>                                                                                                                                                                                                            <lis><img src='/images/mbbs.png' alt='MBBS' onclick='mbbs();' style='width:100px; height:150px;'><br><as href='#'>MBBS</as></lis>                                                                                                                                                                        <lis><img src='/images/law.png' alt='LAW' onclick='law();' style='width:100px; height:150px;'><br><as href='#'>LAW</as></lis>                                                                                                                                                       <lis><img src='/images/school.png' alt='SCHOOL' onclick='school();' style='width:100px; height:150px;'><br><as href='#'>SCHOOL</as></lis>                                                                                                                                                                       <lis><img src='/images/graduation.png' alt='GRADUATION' onclick='graduation();' style='width:100px; height:150px;'><br><as href='#'>GRADUATION</as></lis>                                                                                                                                                                                                   <lis><img src='/images/hotelmanagement.png' alt='HOTEL MANAGEMENT' onclick='hotelmanagement();' style='width:100px; height:150px;'><br><as href='#'>HOTEL<br>MANAGEMENT</as></lis>                                                                              </ul>";
 }
+
 
 
 

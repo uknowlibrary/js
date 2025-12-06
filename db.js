cc5e963057d1b4ec8c4e545ad927218057bdb55a
uknowlibrary <b4548114@gmail.com>
2025-12-06 13:25:49 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,3 +1,15 @@
 function dccnforouzan(){
 location.assign("/library/dccnforouzan5e.html");
 }
+function dbmselmasri(){
+location.assign("/pdf/dbmselmasri.pdf");
+}
+function dmgtgshankerrao(){
+location.assign("/pdf/dmgtgshankerrao.pdf");
+}
+function dmgtoscarlevin(){
+location.assign("/pdf/dmgtoscarlevin.pdf");
+}
+function dmgtrichardjohnsonbaugh(){
+location.assign("/pdf/dmgtrichardjohnsonbaugh.pdf");
+}

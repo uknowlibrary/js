3131552a72dc9d0f255fe332f64680806da936ab
uknowlibrary <b4548114@gmail.com>
2025-12-31 12:38:13 +0530

    Create shorts_database.js

---
+++ shorts_database.js
@@ -0,0 +1,15 @@
+function python(){
+location.assign("/library/pythonshorts.html");
+}
+function java(){
+location.assign("/library/javashorts.html");
+}
+function c(){
+location.assign("/library/cshorts.html");
+}
+function cplus(){
+location.assign("/library/c++shorts.html");
+}
+function kotlin(){
+location.assign("/library/kotlinshorts");
+}
